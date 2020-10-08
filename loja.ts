6027cf53d2d23869d5fab92be857a53ed9745e78
Annehelen-ltda <71146646+Annehelen-ltda@users.noreply.github.com>
2020-10-08 11:32:58 -0300

    TypeS 10 Pass

--- loja.ts
+++ loja.ts
@@ -1,3 +1,6 @@
+function isEmpty(str: string): boolean {
+    let spaceCount = str.length - str.replace(" ", "").length;
+    return str == null || spaceCount == str.length;}
 export class Loja {
 
     constructor(public nome_loja: string, public logradouro: string, public numero: number, public complemento: string,
@@ -6,6 +9,63 @@ export class Loja {
 
     public dados_loja(): string {
         // Implemente aqui
-        return "";
+        if (isEmpty(this.nome_loja)) {
+            throw new Error(`O campo nome da loja é obrigatório`);
+          }
+          if(isEmpty(this.logradouro)){
+            throw new Error(`O campo logradouro do endereço é obrigatório`);
+          }
+          let num = this.numero + ""
+          if(this.numero == 0){
+            num = "s/n";
+          }
+          if(isEmpty(this.municipio)){
+            throw new Error(`O campo município do endereço é obrigatório`);
+          }
+          if(isEmpty(this.estado)){
+            throw new Error(`O campo estado do endereço é obrigatório`);
+          }
+          if(isEmpty(this.cnpj)){
+            throw new Error(`O campo CNPJ da loja é obrigatório`);
+          }
+          if(isEmpty(this.inscricao_estadual)){
+            throw new Error(`O campo inscrição estadual da loja é obrigatório`);
+          }
+        
+          var part2 = `${this.logradouro}, ${num}`;
+          if (! isEmpty (this.complemento)){
+              part2 += ` ${this.complemento}`;
+            }
+          var part3 = "";
+          if (! isEmpty (this.bairro)){
+              part3 += `${this.bairro} - `;
+          }
+          part3 += `${this.municipio} - ${this.estado}`;
+          
+          var part4 = "";
+          if (! isEmpty (this.cep)){
+              part4 = `CEP:${this.cep}`;
+            }
+          if (! isEmpty(this.telefone)){
+              if (! isEmpty(part4)){
+                part4 += ` `;
+              }
+              part4 += `Tel ${this.telefone}`;
+            }
+          if (! isEmpty(part4)){
+              part4 += `\n`;
+            }
+          var part5 = "";
+          if (! isEmpty(this.observacao)){
+              part5 += `${this.observacao}`;
+            }
+          let output = `${this.nome_loja}\n`;
+          output += `${part2}\n`;
+          output += `${part3}\n`;
+          output += `${part4}`;
+          output += `${part5}\n`;
+          output += `CNPJ: ${this.cnpj}\n`;
+          output += `IE: ${this.inscricao_estadual}\n`;
+        return output;
     }
 }
